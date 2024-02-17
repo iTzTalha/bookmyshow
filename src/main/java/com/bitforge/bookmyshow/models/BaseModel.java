4defdef743f52f14039f477c3cfb87cdabe81044
Talha Shaikh <17671511+iTzTalha@users.noreply.github.com>
2024-02-17 21:24:53 +0530

    modified BaseModel.java

--- src/main/java/com/bitforge/bookmyshow/models/BaseModel.java
+++ src/main/java/com/bitforge/bookmyshow/models/BaseModel.java
@@ -1,11 +1,18 @@
 package com.bitforge.bookmyshow.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@MappedSuperclass
 public class BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Date createdOn;
     private Date updatedOn;
