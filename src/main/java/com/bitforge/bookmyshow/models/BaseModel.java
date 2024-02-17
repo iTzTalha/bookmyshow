3126fd595a1be66f3cfa21b3a4528fedba94c918
Talha Shaikh <17671511+iTzTalha@users.noreply.github.com>
2024-02-17 20:22:59 +0530

    modified BaseModel.java

--- src/main/java/com/bitforge/bookmyshow/models/BaseModel.java
+++ src/main/java/com/bitforge/bookmyshow/models/BaseModel.java
@@ -1,12 +1,10 @@
 package com.bitforge.bookmyshow.models;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.util.Date;
 
-@Getter
-@Setter
+@Data
 public class BaseModel {
     private Long id;
     private Date createdOn;
