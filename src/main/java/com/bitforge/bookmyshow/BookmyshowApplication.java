eae2ff9201eb08045ddc19145c2856576dc29fa7
Talha Shaikh <17671511+iTzTalha@users.noreply.github.com>
2024-02-27 16:10:18 +0530

    modified BookmyshowApplication

--- src/main/java/com/bitforge/bookmyshow/BookmyshowApplication.java
+++ src/main/java/com/bitforge/bookmyshow/BookmyshowApplication.java
@@ -1,13 +1,20 @@
 package com.bitforge.bookmyshow;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-public class BookmyshowApplication {
+@EnableJpaAuditing
+public class BookmyshowApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookmyshowApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
