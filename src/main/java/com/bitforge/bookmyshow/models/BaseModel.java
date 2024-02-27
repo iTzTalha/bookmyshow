740550a6870500cfaef5466ce5cb78eed780ca21
Talha Shaikh <17671511+iTzTalha@users.noreply.github.com>
2024-02-27 16:09:56 +0530

    modified BaseModel

--- src/main/java/com/bitforge/bookmyshow/models/BaseModel.java
+++ src/main/java/com/bitforge/bookmyshow/models/BaseModel.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.util.Date;
 
@@ -14,6 +16,8 @@ public class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @CreatedDate
     private Date createdOn;
+    @LastModifiedDate
     private Date updatedOn;
 }
